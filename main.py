829b229c59c5868ef8fcfc35b8ae7c9b9646b029
Tetenkov Alexander <forthe@ya.ru>
2022-10-08 21:50:25 +0300

    first server

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+file_path = "index.html"
+app = FastAPI()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.get("/")
+def main():
+    return FileResponse(path=file_path, filename=file_path, media_type='text/html')
