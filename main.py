5c2feacff9113f43e6ea833382c03535eceb8614
Tetenkov Alexander <forthe@ya.ru>
2022-10-08 22:27:34 +0300

    ws chat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 
 file_path = "index.html"
@@ -8,3 +8,16 @@ app = FastAPI()
 def main():
     with open(file_path, 'r') as file:
         return HTMLResponse(content=file.read())
+
+@app.get("/ws_chat")
+def main():
+    with open('ws_chat.html', 'r') as file:
+        return HTMLResponse(content=file.read())
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
